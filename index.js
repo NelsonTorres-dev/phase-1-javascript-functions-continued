c6cabe842f606ce622d2cb9d75d357f857f886fc
Nelson Torres <ntorres381@gmail.com>
2023-04-07 17:40:24 -0700

    uses default params & anonymous funcs

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(Saturday='roller-skate'){
+    return `This Saturday, I want to ${Saturday}!`
+}
+
+//here we overide the main parameter or activity by using a default parameter as so:
+const mondayWork = function(Monday='go to the office'){
+    return `This Monday, I will ${Monday}.`
+}
+
+function wrapAdjective(highlight="*"){
+        return function(singular = "special"){
+        return `You are ${highlight}${singular}${highlight}!`
+    };
+
+}
+wrapAdjective("*")('a hard worker','!')
